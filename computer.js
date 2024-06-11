9e58c3e21b49aae9d20b9d01c05d99f57aef0dd2
Connective <76740905+Sponton1x@users.noreply.github.com>
2024-06-11 18:43:44 +0200

    Update computer.js

--- computer.js
+++ computer.js
@@ -1,8 +1,10 @@
+// HTML element references
 const board = document.getElementById('board');
-const outputBoard = document.getElementById('output-board');
 const resultsTable = document.getElementById('resultsTable').getElementsByTagName('tbody')[0];
 const summaryTable = document.getElementById('summaryTable').getElementsByTagName('tbody')[0];
-const GameLevel = document.cookie;
+
+let GameLevel = document.cookie.split('; ').find(row => row.startsWith('GameLevel='));
+GameLevel = GameLevel ? GameLevel.split('=')[1] : null;
 
 let currentPlayer = 'X';
 let gameBoard = ['', '', '', '', '', '', '', '', ''];
@@ -11,7 +13,7 @@ let winCross = 0;
 let winCircle = 0;
 let draws = 0;
 
-function checkWinner() {
+function checkWinner(board) {
     const winPatterns = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
         [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
@@ -20,56 +22,68 @@ function checkWinner() {
 
     for (const pattern of winPatterns) {
         const [a, b, c] = pattern;
-        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
-            return gameBoard[a];
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            return board[a];
         }
     }
 
     return null;
 }
 
-function checkDraw() {
-    return !gameBoard.includes('');
+function checkDraw(board) {
+    return !board.includes('');
 }
 
 function updateCurrentPlayerText() {
-    const currentPlayerText = document.getElementById("currentPlayer-text")
-    currentPlayerText.textContent = `Teraz gracz ${currentPlayer}`
+    const currentPlayerText = document.getElementById("currentPlayer-text");
+    currentPlayerText.textContent = `Teraz gracz ${currentPlayer}`;
 }
 
-// When human win
 function handleCellClick(index) {
     if (gameBoard[index] || gameOver) return;
 
     gameBoard[index] = currentPlayer;
     renderBoard();
-    updateCurrentPlayerText();
-
-    const winner = checkWinner();
-    const draw = checkDraw();
+    const winner = checkWinner(gameBoard);
+    const draw = checkDraw(gameBoard);
 
     if (winner) {
         alert(`Gracz ${winner} wygrał!`);
         gameOver = true;
-        updateCurrentPlayerText();
         addResult(`Gracz ${winner}`);
         handleGameEnd();
     } else if (draw) {
         alert('Jest remis!');
         gameOver = true;
-        updateCurrentPlayerText();
         addResult('Remis');
         handleGameEnd();
     } else {
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
         updateCurrentPlayerText();
 
-        // Computer move after player move
-        if (currentPlayer == 'O' && !gameOver) {
-            if (GameLevel === "mode: hard") {
-                minimax();
-            }
-            else if (GameLevel === "mode: easy") {
+        if (currentPlayer === 'O' && !gameOver) {
+            if (GameLevel === 'mode: hard') {
+                const bestMove = minimax(gameBoard, true).move;
+                gameBoard[bestMove] = currentPlayer;
+                renderBoard();
+                const winner = checkWinner(gameBoard);
+                const draw = checkDraw(gameBoard);
+
+                if (winner) {
+                    alert(`Gracz ${winner} wygrał!`);
+                    gameOver = true;
+                    addResult(`Gracz ${winner}`);
+                    handleGameEnd();
+                } else if (draw) {
+                    alert('Jest remis!');
+                    gameOver = true;
+                    addResult('Remis');
+                    handleGameEnd();
+                } else {
+                    currentPlayer = 'X';
+                    updateCurrentPlayerText();
+                }
+            } else {
                 makeComputerMove();
             }
         }
@@ -84,98 +98,69 @@ function renderBoard() {
         cell.addEventListener('click', () => handleCellClick(i));
         board.appendChild(cell);
     }
+    updateCurrentPlayerText();
 }
 
-// When computer win
 function makeComputerMove() {
-    const avaliableMoves = gameBoard.reduce((acc, cell, index) => {
-        // Check avability cells
-        if (cell == '') {
-            acc.push(index);
-        }
-
-        return acc
+    const availableMoves = gameBoard.reduce((acc, cell, index) => {
+        if (cell === '') acc.push(index);
+        return acc;
     }, []);
 
-    // Check avability moves
-    if (avaliableMoves.length == 0) {
-        return;
-    }
+    if (availableMoves.length === 0) return;
 
-    const randomIndex = Math.floor(Math.random() * avaliableMoves.length);
-    const computerMove = avaliableMoves[randomIndex];
+    const randomIndex = Math.floor(Math.random() * availableMoves.length);
+    const computerMove = availableMoves[randomIndex];
 
     setTimeout(() => {
         gameBoard[computerMove] = currentPlayer;
         renderBoard();
-        updateCurrentPlayerText();
-
-        const winner = checkWinner();
-        const draw = checkDraw();
+        const winner = checkWinner(gameBoard);
+        const draw = checkDraw(gameBoard);
 
         if (winner) {
             alert(`Gracz ${winner} wygrał!`);
             gameOver = true;
-            updateCurrentPlayerText();
             addResult(`Gracz ${winner}`);
             handleGameEnd();
         } else if (draw) {
             alert('Jest remis!');
             gameOver = true;
-            updateCurrentPlayerText();
-            addResult(`Remis`);
+            addResult('Remis');
             handleGameEnd();
         } else {
-            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            currentPlayer = 'X';
             updateCurrentPlayerText();
         }
-    }, 300); // Set delay for computer moves for better visualluzation
+    }, 300);
 }
 
-// Harder mode than used random generated index
 function minimax(board, maxPlayer) {
+    const winner = checkWinner(board);
+    if (winner === 'O') return { score: 1 };
+    if (winner === 'X') return { score: -1 };
+    if (checkDraw(board)) return { score: 0 };
+
     const currentPlayer = maxPlayer ? 'O' : 'X';
-
-    if (winner === 'O') {
-        return { score: 1 }
-    }
-    else if (winner === 'X') {
-        return { score: -1 }
-    }
-    else if (checkDraw()) {
-        return { score: 0 }
-    }
-
-    const avaliablesMoves = [];
+    const availableMoves = [];
     for (let i = 0; i < board.length; i++) {
-        if (board[i] === ''){
-            avaliablesMoves.push(i);
-        }
+        if (board[i] === '') availableMoves.push(i);
     }
 
-    // Recursion
-    const moves = [];
-    for (let i = 0; i < avaliablesMoves.length; i++) {
-        const move = avaliablesMoves[i];
-        const newBoard = [...board];    
+    const moves = availableMoves.map(move => {
+        const newBoard = [...board];
         newBoard[move] = currentPlayer;
-        
         const result = minimax(newBoard, !maxPlayer);
-        moves.push({ move, score: result.score });
-    }
+        return { move, score: result.score };
+    });
 
-    // Choice best move for player
-    let bestMove;
     if (maxPlayer) {
-        const maxScore = Math.max(...moves.map((m) => m.score));
-        bestMove = moves.find((m) => m.score === maxScore);
+        const maxScore = Math.max(...moves.map(m => m.score));
+        return moves.find(m => m.score === maxScore);
+    } else {
+        const minScore = Math.min(...moves.map(m => m.score));
+        return moves.find(m => m.score === minScore);
     }
-    else {
-        const minScore = Math.min(...moves.map((m) => m.score));
-        bestMove = moves.find((m) => m.score === minScore);
-    }
-
-    return bestMove;
 }
 
 function resetGame() {
@@ -183,39 +168,20 @@ function resetGame() {
     gameBoard = ['', '', '', '', '', '', '', '', ''];
     gameOver = false;
     renderBoard();
-    updateCurrentPlayerText();
 }
 
 function addResult(result) {
-    // Results Table
     const resultsRow = resultsTable.insertRow();
     const resultCell = resultsRow.insertCell(0);
     resultCell.textContent = result;
-
-    // Summary Table
-    const summaryRow = summaryTable.insertRow();
-    const summaryCellCross = summaryRow.insertCell(0);
-    const summaryCellCircle = summaryRow.insertCell(1);
-    const summaryCellDraw = summaryRow.insertCell(2);
-    summaryCellCross.textContent = winCross;
-    summaryCellCircle.textContent = winCircle;
-    summaryCellDraw.textContent = draws;
-
-    // Save data to localStorage
     saveResultsToLocalStorage();
 }
 
 function saveResultsToLocalStorage() {
-    const summaryData = {
-        cross: winCross,
-        circle: winCircle,
-        draw: draws
-    };
-
+    const summaryData = { cross: winCross, circle: winCircle, draw: draws };
     const resultsData = [];
     for (let i = 0; i < resultsTable.rows.length; i++) {
-        const result = resultsTable.rows[i].cells[0].textContent;
-        resultsData.push({ result });
+        resultsData.push({ result: resultsTable.rows[i].cells[0].textContent });
     }
     localStorage.setItem('summaryData', JSON.stringify(summaryData));
     localStorage.setItem('resultsData', JSON.stringify(resultsData));
@@ -225,40 +191,26 @@ function loadResultsFromLocalStorage() {
     const summaryDataString = localStorage.getItem('summaryData');
     if (summaryDataString) {
         const summaryData = JSON.parse(summaryDataString);
-
-        if (
-            summaryData &&
-            typeof summaryData.cross === 'number' &&
-            typeof summaryData.circle === 'number' &&
-            typeof summaryData.draw === 'number'
-        ) {
-            winCross = summaryData.cross;
-            winCircle = summaryData.circle;
-            draws = summaryData.draw;
-
-            updateSummaryTable();
-        } else {
-            console.error('Incorrect type data in LocalStorage.');
-        }
+        winCross = summaryData.cross;
+        winCircle = summaryData.circle;
+        draws = summaryData.draw;
+        updateSummaryTable();
     }
 
     const resultsDataString = localStorage.getItem('resultsData');
     if (resultsDataString) {
         const resultsData = JSON.parse(resultsDataString);
         resultsTable.innerHTML = '';
-
-        for (const { result } of resultsData) {
+        resultsData.forEach(({ result }) => {
             const newRow = resultsTable.insertRow();
             const cell1 = newRow.insertCell(0);
             cell1.textContent = result;
-        }
+        });
     }
 }
 
 function updateSummaryTable() {
-    // Summary Table
     summaryTable.innerHTML = '';
-
     const summaryRow = summaryTable.insertRow();
     const summaryCellCross = summaryRow.insertCell(0);
     const summaryCellCircle = summaryRow.insertCell(1);
@@ -266,26 +218,26 @@ function updateSummaryTable() {
     summaryCellCross.textContent = winCross;
     summaryCellCircle.textContent = winCircle;
     summaryCellDraw.textContent = draws;
-
-    // Save data to localStorage
     saveResultsToLocalStorage();
 }
 
 function handleGameEnd() {
-    if (checkWinner() === 'O') {
-        winCircle++;
-    } else if (checkWinner() === 'X') {
-        winCross++;
-    } else if (checkDraw()) {
-        draws++;
-    }
-
+    const winner = checkWinner(gameBoard);
+    if (winner === 'O') winCircle++;
+    if (winner === 'X') winCross++;
+    if (checkDraw(gameBoard)) draws++;
     updateSummaryTable();
-    resetGame()
+    resetGame();
 }
 
-function choiceGameLevel() {
-
+function choiceGameLevel(level) {
+    if (level !== "selector") {
+        GameLevel = `mode: ${level}`;
+        document.cookie = `GameLevel=${GameLevel};path=/`;
+        alert(`Wybrano poziom trudności: ${level}`);
+        board.hidden = false; // Show the board
+        renderBoard();
+    }
 }
 
 function resetTable() {
@@ -299,7 +251,19 @@ function resetTable() {
 }
 
 window.onload = function () {
+    resetTable();
+    resetGame();
+    if (GameLevel && GameLevel !== 'mode: selector') {
+        board.hidden = false;
+        renderBoard();
+    }
     loadResultsFromLocalStorage();
-    renderBoard();
     updateCurrentPlayerText();
+
+    const gameLevelSelect = document.getElementById('gameLevel');
+    if (GameLevel.includes('easy')) {
+        gameLevelSelect.value = 'easy';
+    } else if (GameLevel.includes('hard')) {
+        gameLevelSelect.value = 'hard';
+    }
 }
